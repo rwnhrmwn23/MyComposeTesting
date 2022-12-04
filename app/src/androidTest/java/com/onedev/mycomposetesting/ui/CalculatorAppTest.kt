791ef93d6a3af73004d452cbf785f4e59d7b9318
Irwan Hermawan <rwnhermawan@gmail.com>
2022-12-04 15:18:02 +0700

    UI Testing with createComposeAndroidRule

--- app/src/androidTest/java/com/onedev/mycomposetesting/ui/CalculatorAppTest.kt
+++ app/src/androidTest/java/com/onedev/mycomposetesting/ui/CalculatorAppTest.kt
@@ -1,21 +1,46 @@
 package com.onedev.mycomposetesting.ui
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.onedev.mycomposetesting.R
 import com.onedev.mycomposetesting.ui.theme.MyComposeTestingTheme
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class CalculatorAppTest {
+//    @get:Rule
+//    val composeTestRule = createComposeRule()
+//
+//    @Before
+//    fun setUp() {
+//        composeTestRule.setContent {
+//            MyComposeTestingTheme {
+//                CalculatorApp()
+//            }
+//        }
+//    }
+//
+//    @Test
+//    fun calculate_area_of_rectangle_correct() {
+//        composeTestRule.apply {
+//            onNodeWithText("Masukkan panjang").performTextInput("3")
+//            onNodeWithText("Masukkan lebar").performTextInput("4")
+//            onNodeWithText("Hitung!").performClick()
+//            onNodeWithText("Hasil: 12.0").assertExists()
+//        }
+//    }
+
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestAndroidRule = createAndroidComposeRule<ComponentActivity>()
 
     @Before
     fun setUp() {
-        composeTestRule.setContent {
+        composeTestAndroidRule.setContent {
             MyComposeTestingTheme {
                 CalculatorApp()
             }
@@ -23,12 +48,22 @@ class CalculatorAppTest {
     }
 
     @Test
-    fun calculate_area_of_rectangle_correct() {
-        composeTestRule.apply {
-            onNodeWithText("Masukkan panjang").performTextInput("3")
-            onNodeWithText("Masukkan lebar").performTextInput("4")
-            onNodeWithText("Hitung!").performClick()
-            onNodeWithText("Hasil: 12.0").assertExists()
+    fun calculate_area_of_rectangle_correct_with_compose_test_android_rule() {
+        composeTestAndroidRule.apply {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, 12.0)).assertExists()
+        }
+    }
+
+    @Test
+    fun wrong_input_not_calculated() {
+        composeTestAndroidRule.apply {
+            onNodeWithText(activity.getString(R.string.enter_length)).performTextInput("..3")
+            onNodeWithText(activity.getString(R.string.enter_width)).performTextInput("4")
+            onNodeWithText(activity.getString(R.string.count)).performClick()
+            onNodeWithText(activity.getString(R.string.result, 0.0)).assertExists()
         }
     }
 }
